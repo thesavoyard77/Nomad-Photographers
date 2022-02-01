02f277a3ba1426f8ca8a497426879d49ca1dca8a
chris <felix.christopher.e@gmail.com>
2022-02-01 15:34:45 -0800

    removed env inhection

--- react-app/config/webpack.config.js
+++ react-app/config/webpack.config.js
@@ -231,11 +231,6 @@ module.exports = function (webpackEnv) {
       // module chunks which are built will work in web workers as well.
       globalObject: 'this',
     },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env.REACT_APP_MAPS_KEY': JSON.stringify(process.env.REACT_APP_MAPS_KEY),
-      }),
-    ],
     optimization: {
       minimize: isEnvProduction,
       minimizer: [
