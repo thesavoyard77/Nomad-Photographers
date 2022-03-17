660db1671d958bb32db43831e3ee284244e98fc0
chris <felix.christopher.e@gmail.com>
2022-03-16 23:17:50 -0700

    it's my only day off and Game of Thrones calls

--- react-app/src/components/Test/Comments/Comments.js
+++ react-app/src/components/Test/Comments/Comments.js
@@ -7,23 +7,21 @@ export default function Comments ({ photo }) {
     const photo_id = photo?.id
 
     const rotateComments = (index) => {
-        if (commentsArr.length < 1) return <div className="no-comments"><h3>Be the first to comment</h3></div>
+
         if (index % 2 === 0) {
             return (
-                <div className='comment-div'key={commentsArr[index]?.id} >
-                    <hr className="comments-hr"></hr>
-                    {console.log(commentsArr[index])}
-                    <h3 className="comments-username">{commentsArr[index]?.user_name?.username.split("")[0].toUpperCase() + commentsArr[index]?.user_name?.username.slice(1)}</h3>
-                    <p className="comment-body"><b>{commentsArr[index]?.body}</b></p>
+                <div className='even-comment-div'key={commentsArr[index]?.id} >
+                    <hr className="even-comments-hr"></hr>
+                    <h3 className="even-comments-username">{commentsArr[index]?.user_name?.username.split("")[0].toUpperCase() + commentsArr[index]?.user_name?.username.slice(1)}</h3>
+                    <p className="even-comment-body"><b>{commentsArr[index]?.body}</b></p>
                 </div>
             )
         } else {
             return (
-                <div className='comment-div'key={commentsArr[index]?.id} >
-                    <hr className="comments-hr"></hr>
-                    {console.log(commentsArr[index])}
-                    <h3 className="comments-username">{commentsArr[index]?.user_name?.username.split("")[0].toUpperCase() + commentsArr[index]?.user_name?.username.slice(1)}</h3>
-                    <p className="comment-body"><b>{commentsArr[index]?.body}</b></p>
+                <div className='odd-comment-div'key={commentsArr[index]?.id} >
+                    <hr className="odd-comments-hr"></hr>
+                    <h3 className="odd-comments-username">{commentsArr[index]?.user_name?.username.split("")[0].toUpperCase() + commentsArr[index]?.user_name?.username.slice(1)}</h3>
+                    <p className="odd-comment-body"><b>{commentsArr[index]?.body}</b></p>
                 </div>
             )
         }
@@ -38,8 +36,3 @@ export default function Comments ({ photo }) {
     )
 }
 
-{/* <div className='comment-div' key={comment?.id}>
-<hr className="comments-hr"></hr>
-<h3 className="comments-username">{comment?.user_name?.username.split("")[0].toUpperCase() + comment?.user_name?.username.slice(1)}</h3>
-<p className="comment-body"><b>{comment?.body}</b></p>
-</div> */}
